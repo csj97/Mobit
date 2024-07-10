111eddcd9c4528c102ad42f41144fbd0cb5c0757
csj97 <whtjdwo78@openobject.net>
2024-07-10 11:03:08 +0900

    Initial Commit

---
+++ Mobit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mobit
+//
+//  Created by 조성재 on 7/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
