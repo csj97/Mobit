5df0a6fc534aee4735fe521b9f6193c58f32c9a9
sungjae <whtjdwo78@hanpass.com>
2025-01-05 17:31:20 +0900

    [Edit] MainCoordinator 수정
    1. 메인 vc로 돌아오면 다시 웹소켓 연결

--- Mobit/Presentation/MainScene/Coordinator/MainCoordinator.swift
+++ Mobit/Presentation/MainScene/Coordinator/MainCoordinator.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MainCoordinator: BaseCoordinator {
+class MainCoordinator: NSObject, BaseCoordinator, UINavigationControllerDelegate {
   var childCoordinators = [BaseCoordinator]()
   var navigationController: UINavigationController
   
@@ -17,6 +17,7 @@ class MainCoordinator: BaseCoordinator {
   }
   
   func start() {
+    self.navigationController.delegate = self
     let reactor = MainReactor(mainUseCase: MainUseCase(mainRepository: MainRepository()))
     let mainVC = MainViewController(reactor: reactor)
     mainVC.coordinator = self
@@ -38,5 +39,16 @@ class MainCoordinator: BaseCoordinator {
   /// 관심 코인 목록 탭 노출
   func showFavoriteCoinList() {
     
+  }
+  
+  func navigationController(
+    _ navigationController: UINavigationController,
+    didShow viewController: UIViewController,
+    animated: Bool
+  ) {
+    // 뒤로가기 이 후, mainVC로 돌아왔다면, 다시 socket 연결
+    if let mainVC = viewController as? MainViewController {
+      mainVC.reactor.action.onNext(.loadCrypto(selectedTab: .krw))
+    }
   }
 }
