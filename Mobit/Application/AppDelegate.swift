521b9ea6f4650d734fe0126ff4f5498556891136
sungjae <whtjdwo78@hanpass.com>
2025-07-25 16:42:28 +0900

    [Edit] 세로 방향으로 고정되게 수정

--- Mobit/Application/AppDelegate.swift
+++ Mobit/Application/AppDelegate.swift
@@ -15,7 +15,7 @@ import FirebaseCore
 class AppDelegate: UIResponder, UIApplicationDelegate {
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    
+	
 	FirebaseApp.configure()
 	
 	MobileAds.shared.start(completionHandler: nil)
@@ -26,28 +26,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	]
 	
 	requestATT()
-    checkFirstLaunch()
-    return true
+	checkFirstLaunch()
+	return true
   }
-
+  
   // MARK: UISceneSession Lifecycle
-
+  
   func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+	// Called when a new scene session is being created.
+	// Use this method to select a configuration to create the new scene with.
+	return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
   }
-
+  
   func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+	// Called when the user discards a scene session.
+	// If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+	// Use this method to release any resources that were specific to the discarded scenes, as they will not return.
   }
-
+  
+  /// 세로 방향 고정
+  func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+	return UIInterfaceOrientationMask.portrait
+  }
+  
   /// App의 첫 실행 여부
   func checkFirstLaunch() {
-    // 첫 실행이면 천만원 설정
-    if UserDataManager.isFirstLaunch {
+	// 첫 실행이면 천만원 설정
+	if UserDataManager.isFirstLaunch {
 	  UserDataManager.isFirstLaunch = false
 	  UserDataManager.userInformation = MobitUserInformation(
 		userAvailableBalance: 10_000_000
