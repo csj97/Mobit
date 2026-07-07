9b591116797644410c165fe460a2a0573916d807
sungjae <whtjdwo78@hanpass.com>
2026-07-07 14:34:14 +0900

    [Edit] splash 화면 변경

--- Mobit/Resource/MobitLaunchScreen.swift
+++ Mobit/Resource/MobitLaunchScreen.swift
@@ -10,12 +10,6 @@ import UIKit
 
 class MobitLaunchScreen: UIViewController {
   
-  @IBOutlet weak var label1: UILabel!
-  @IBOutlet weak var label2: UILabel!
-  @IBOutlet weak var mobitNameLabel: UILabel!
-  @IBOutlet weak var label3: UILabel!
-  @IBOutlet weak var label4: UILabel!
-  
   override func viewDidLoad() {
 	super.viewDidLoad()
 //	for family in UIFont.familyNames {
@@ -24,10 +18,5 @@ class MobitLaunchScreen: UIViewController {
 //		print("   - \(name)")
 //	  }
 //	}
-	mobitNameLabel.font = UIFont(name: "Partial-Sans-KR", size: 25)
-	label1.font = UIFont(name: "esamanruOTFMedium", size: 18)
-	label2.font = UIFont(name: "esamanruOTFLight", size: 16)
-	label3.font = UIFont(name: "esamanruOTFLight", size: 16)
-	label4.font = UIFont(name: "esamanruOTFLight", size: 14)
   }
 }
